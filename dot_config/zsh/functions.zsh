223c038b2d3d5562f5c71425f0c6133c305d8607
Alwyn Kik <alwyn@kik.pw>
2024-02-05 21:31:32 +0100

    Add .config/zsh/aliases.zsh Add .config/zsh/completions.zsh Add .config/zsh/.zcompdump Add .config/zsh/.zshenv Add .config/zsh/.zshrc Add .config/zsh/.zshrc.bak Add .config/zsh/env.zsh Add .config/zsh/functions.zsh Add .config/zsh/history.zsh Add .config/zsh/keybinds.zsh Add .config/zsh/.zsh_history Add .config/zsh/tools.zsh

---
+++ dot_config/zsh/functions.zsh
@@ -0,0 +1,40 @@
+####################
+# CUSTOM FUNCTIONS #
+####################
+
+# ripgrep with fzf
+function frg {
+  result=$(rg --ignore-case --color=always --line-number --no-heading "$@" |
+    fzf --ansi \
+        --color 'hl:-1:underline,hl+:-1:underline:reverse' \
+        --delimiter ':' \
+        --preview "bat --color=always {1} --theme='Solarized (light)' --highlight-line {2}" \
+        --preview-window 'up,60%,border-bottom,+{2}+3/3,~3')
+  file=${result%%:*}
+  linenumber=$(echo "${result}" | cut -d: -f2)
+  if [[ -n "$file" ]]; then
+    $EDITOR +"${linenumber}" "$file"
+  fi
+}
+
+# Interactive shutdown/reboot
+custom_shutdown() {
+  read "confirm?Do you really want to ${1}? "
+  if [[ "$confirm" =~ ^[Yy]$ ]]
+  then
+    "$1"
+  fi
+}
+
+# Default DPI
+dedpi() {
+  sed -i 's/155648/98304/' $HOME/.xsettingsd
+  /usr/bin/xrdb -merge $HOME/.Xresources
+}
+
+# HiDPI
+hidpi() {
+  sed -i 's/98304/155648/' $HOME/.xsettingsd
+  /usr/bin/xrdb -merge $HOME/.Xresources.hidpi
+}
+
