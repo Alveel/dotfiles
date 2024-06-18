02ceeb4ec431db5dcc39ddbdf3229e039f6f7002
Alwyn Kik <alwyn@kik.pw>
2024-06-18 16:42:13 +0200

    Update .config/zsh/env.zsh

--- dot_config/zsh/env.zsh
+++ dot_config/zsh/env.zsh
@@ -3,4 +3,5 @@ export FZF_PREVIEW_ADVANCED=true
 export LESSOPEN='| lessfilter-fzf %s'
 export ZSH_EXTEND_HISTORY_FILE="$ZDOTDIR/.zsh_extended_history"
 export MC_DISABLE_PAGER=true
+export KREW_NO_UPGRADE_CHECK=1
 
